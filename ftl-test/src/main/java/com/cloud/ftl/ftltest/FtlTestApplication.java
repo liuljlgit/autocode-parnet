f32d6a6e3219a4d9aa4ab583aa42d68344f94304
liulijun <liulj@huidiancloud.com>
2019-04-04 15:20:23 +0800

    加入dao目录

--- ftl-test/src/main/java/com/cloud/ftl/ftltest/FtlTestApplication.java
+++ ftl-test/src/main/java/com/cloud/ftl/ftltest/FtlTestApplication.java
@@ -1,11 +1,13 @@
 package com.cloud.ftl.ftltest;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.cloud")
+@MapperScan("com.cloud.**.dao")
 public class FtlTestApplication {
 
 	public static void main(String[] args) {
