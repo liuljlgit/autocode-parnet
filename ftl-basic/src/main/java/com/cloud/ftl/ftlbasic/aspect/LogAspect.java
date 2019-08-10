34b67da163b38af05d57739e712ecb469ae38c8d
yizhimanong <lijunzhangdale@163.com>
2019-08-10 11:15:44 +0800

    优化切面

--- ftl-basic/src/main/java/com/cloud/ftl/ftlbasic/aspect/LogAspect.java
+++ ftl-basic/src/main/java/com/cloud/ftl/ftlbasic/aspect/LogAspect.java
@@ -2,16 +2,15 @@ package com.cloud.ftl.ftlbasic.aspect;
 
 import com.cloud.ftl.ftlbasic.utils.RegxUtil;
 import com.cloud.ftl.ftlbasic.webEntity.BaseQuery;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
 @Aspect
+@Slf4j
 @Component
 public class LogAspect {
 
@@ -19,18 +18,7 @@ public class LogAspect {
     public void annotationPoinCut(){}
 
     @Before("annotationPoinCut()")
-    public void before(JoinPoint joinPoint) throws Exception {
-        System.out.println("-------sdsad");
-        Object[] args = joinPoint.getArgs();
-        if(args.length != 1){
-            return;
-        }
-        Object target = args[0];
-        Class<?> tClass = target.getClass();
-        if(tClass.getSuperclass() != BaseQuery.class){
-            return;
-        }
-        Field[] fields = tClass.getDeclaredFields();
-        Method acMethod = tClass.getSuperclass().getDeclaredMethod("andCriteria", null);
+    public void before(JoinPoint joinPoint) {
+        log.info("-------- LogAspect Aspect ---------");
     }
 }
