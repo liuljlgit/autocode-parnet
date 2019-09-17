276d6ba6f902eaf004524e30e70ca8dadd01401e
liulijun <liulj@huidiancloud.com>
2019-09-17 12:00:01 +0800

    优化生成代码生成器的工具类方法

--- ftl-basic/src/main/java/com/cloud/ftl/ftlbasic/func/OptSupplier.java
+++ ftl-basic/src/main/java/com/cloud/ftl/ftlbasic/func/OptSupplier.java
@@ -4,7 +4,7 @@ package com.cloud.ftl.ftlbasic.func;
  * 回调函数入参
  * @param <R>
  */
-public interface Func0<R> {
+public interface OptSupplier<R> {
 
     /**
      * 执行函数
@@ -12,6 +12,6 @@ public interface Func0<R> {
      * @return 函数执行结果
      * @throws Exception 自定义异常
      */
-    R call() throws Exception;
+    R call(R r) throws Exception;
 
 }
--- ftl-basic/src/main/java/com/cloud/ftl/ftlbasic/utils/MapUtil.java
+++ ftl-basic/src/main/java/com/cloud/ftl/ftlbasic/utils/MapUtil.java
@@ -1,7 +1,7 @@
 package com.cloud.ftl.ftlbasic.utils;
 
-import com.cloud.ftl.ftlbasic.func.Func0;
-import com.cloud.ftl.ftlbasic.func.Func1;
+import com.cloud.ftl.ftlbasic.func.InitSupplier;
+import com.cloud.ftl.ftlbasic.func.OptSupplier;
 import lombok.extern.slf4j.Slf4j;
 import java.util.Map;
 import java.util.Objects;
@@ -17,16 +17,16 @@ public class MapUtil {
      * 从map中获取值，如果没有则设定一个默认值
      * @param map
      * @param key
-     * @param supplier
+     * @param initSupplier
      * @param <K>
      * @param <V>
      * @return
      */
-    public static <K,V> V getOrPutDefault(Map<K,V> map, K key, Func0<V> supplier) {
+    public static <K,V> V getOrPutDefault(Map<K,V> map, K key, InitSupplier<V> initSupplier) {
         try {
             V defaultVal = map.getOrDefault(key,null);
             if(Objects.isNull(defaultVal)){
-                V call = supplier.call();
+                V call = initSupplier.call();
                 map.putIfAbsent(key,call);
                 return call;
             }
@@ -41,19 +41,19 @@ public class MapUtil {
      * 设置默认值，设置操作，返回新值
      * @param map 需要操作的map
      * @param key 需要操作的key
-     * @param supplier 设置操作回调方法
+     * @param optSupplier 设置操作回调方法
      * @param <K> key的类型
      * @param <V> value的类型
      * @return 操作之后返回的新值
      */
-    public static <K,V> V getHandleSetVal(Map<K,V> map,K key, Func1<V> supplier){
+    public static <K,V> V getHandleSetVal(Map<K,V> map,K key, OptSupplier<V> optSupplier){
         try {
             V oldVal;
             oldVal = map.getOrDefault(key,null);
             if(Objects.isNull(oldVal)){
                 return null;
             }
-            V call = supplier.call(oldVal);
+            V call = optSupplier.call(oldVal);
             map.put(key,call);
             return oldVal;
         } catch (Exception e) {
@@ -66,20 +66,20 @@ public class MapUtil {
      * 设置默认值，设置操作，返回新值
      * @param map 需要操作的map
      * @param key 需要操作的key
-     * @param supplier0 设置默认值回调方法
-     * @param supplier1 设置操作回调方法
+     * @param initSupplier 设置默认值回调方法
+     * @param optSupplier 设置操作回调方法
      * @param <K> key的类型
      * @param <V> value的类型
      * @return 操作之后返回的新值
      */
-    public static <K,V> V getHandleSetVal(Map<K,V> map,K key, Func0<V> supplier0,Func1<V> supplier1){
+    public static <K,V> V getHandleSetVal(Map<K,V> map, K key, InitSupplier<V> initSupplier, OptSupplier<V> optSupplier){
         try {
             V oldVal;
             oldVal = map.getOrDefault(key,null);
             if(Objects.isNull(oldVal)){
-                oldVal = getOrPutDefault(map, key, supplier0);
+                oldVal = getOrPutDefault(map, key, initSupplier);
             }
-            V call = supplier1.call(oldVal);
+            V call = optSupplier.call(oldVal);
             map.put(key,call);
             return oldVal;
         } catch (Exception e) {
